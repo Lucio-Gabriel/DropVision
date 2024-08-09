f7e542f8330de266e35af3fa672f6f0b096e173b
Gabriel <suppershock777@gmail.com>
2024-08-08 21:50:55 -0300

    Feat: criado card de acesso ao sistema

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <form method="post" action="table_data.php">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="stylesheet" href="StyleComponets/stylebtn.css">
+    <h1 class="text-center">Seja Bem Vindo a DropVision</h1>
 
-    <h1>Cadastra Usuário:</h1>
+    <style>
+        body{
+            background-color: #9D06FF;
+        }
+    </style>
 
-    <label>
-        Nome:</br>
-        <input type="text" name="name"/>
-    </label></br></br>
+   
 
-    <label>
-        E-mail:</br>
-        <input type="email" name="email"/>
-    </label></br></br>
-
-    <input type="submit" value="Registrar"/>
+<div class="text-center d-flex justify-content-center align-items-center min-vh-100">
+        <div class="card" style="width: 18rem;">
+            <img src="scr/DropVisionLogo.png" width="300" height="300" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Bem Vindo</h5>
+                <p class="card-text">O melhor site para visualizar seus concorrentes de Dropshipping.</p>
+                <a href="table_data.php" class="btn btn-primary">Acessar Sistema</a>
+            </div>
+        </div>
+    </div>
 
 </form>
